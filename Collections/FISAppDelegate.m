4d4a0c031c39f67747afd960f4ab657844f829eb
Rama Milaneh <ramamilaneh@gmail.com>
2017-02-10 14:11:26 -0500

    Done

--- Collections/FISAppDelegate.m
+++ Collections/FISAppDelegate.m
@@ -10,10 +10,163 @@
     return YES;
 }
 
-/**
+-(NSArray *)arrayBySortingArrayAscending:(NSArray *)array {
+    NSSortDescriptor *sortAsc = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES];
+    return [array sortedArrayUsingDescriptors:@[sortAsc]];
+}
+-(NSArray *)arrayBySortingArrayDescending:(NSArray *)array {
+    NSSortDescriptor *sortDes = [NSSortDescriptor sortDescriptorWithKey:nil ascending:NO];
+    
+    return [array sortedArrayUsingDescriptors:@[sortDes]];
+}
+-(NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)array {
+    NSMutableArray *new = [array mutableCopy];
+    NSUInteger endIndex = [new count] -1;
+    [new exchangeObjectAtIndex:0 withObjectAtIndex:endIndex];
+    return [NSArray arrayWithArray:new];
+
+}
+-(NSArray *)arrayByReversingArray:(NSArray *)array {
+    NSMutableArray *new = [array mutableCopy];
+    NSUInteger endIndex = [new count]/2 ;
+    NSUInteger lastIndex = [new count] - 1;
+    for (NSUInteger i = 0; i< endIndex; i++) {
+        [new exchangeObjectAtIndex:i withObjectAtIndex:lastIndex-i];
+    }
+    return new;
  
- * Define your methods here.
+}
+
+-(NSString *)stringInBasicLeetFromString:(NSString *)string {
+    NSDictionary *conversions = @{ @"a" : @"4" ,
+                                   @"s" : @"5" ,
+                                   @"i" : @"1" ,
+                                   @"l" : @"1" ,
+                                   @"e" : @"3" ,
+                                   @"t" : @"7" };
+    NSString *leetString = string;
+    
+    for (NSString *key in conversions) {
+        leetString = [leetString stringByReplacingOccurrencesOfString:key withString:conversions[key]];
+    }
+    
+    return leetString;
+}
+-(NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)array {
+    NSMutableArray *negativeNumbers = [[NSMutableArray alloc] init];
+    NSMutableArray *positiveNumbers = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i<[array count]; i++) {
+        if([array[i] floatValue] >= 0) {
+            [positiveNumbers addObject:array[i]];
+        }else{
+            [negativeNumbers addObject:array[i]];
+        }
+    }
+    return @[negativeNumbers,positiveNumbers];
+
+}
+-(NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)dictionary {
+    NSMutableArray *new = [[NSMutableArray alloc] init];
+    for(NSString *key in dictionary ) {
+        if([dictionary[key] isEqualToString:@"hobbit"]) {
+            [new addObject:key];
+        }
+    }
+    
+    return new;
+  
+}
+-(NSArray *)stringsBeginningWithAInArray:(NSArray *)array {
+    NSPredicate *beginsWithAPredicate = [NSPredicate predicateWithFormat:@"self BEGINSWITH[c] 'a'"];
+    NSArray *beginsWithA = [array filteredArrayUsingPredicate:beginsWithAPredicate];
+    
+    return beginsWithA;
+   
+}
+
+-(NSInteger)sumOfIntegersInArray:(NSArray *)array {
+    NSInteger sum = 0;
+    for (NSNumber *number in array) {
+        sum = sum + [number integerValue];
+    }
+    
+    return sum;
+}
+-(NSArray *)arrayByPluralizingStringsInArray:(NSArray *)array {
+    NSMutableArray *plurals = [[NSMutableArray alloc] init];
+    
+    for (NSString *singular in array) {
+        NSString *plural = @"";
+        if ([singular containsString:@"oo"]) {
+            plural = [singular stringByReplacingOccurrencesOfString:@"oo" withString:@"ee"];
+        } else if ([singular containsString:@"ox"]) {
+            if ([singular hasPrefix:@"b"]) {
+                plural = [singular stringByAppendingString:@"es"];
+            } else {
+                plural = [singular stringByAppendingString:@"en"];
+            }
+        } else if ([singular hasSuffix:@"us"]) {
+            plural = [singular stringByReplacingOccurrencesOfString:@"us" withString:@"i"];
+        } else if ([singular hasSuffix:@"um"]) {
+            plural = [singular stringByReplacingOccurrencesOfString:@"um" withString:@"a"];
+        } else {
+            plural = [singular stringByAppendingString:@"s"];
+        }
+        [plurals addObject:plural];
+    }
+    
+    return [NSArray arrayWithArray:plurals];
  
- */
+}
+-(NSDictionary *)countsOfWordsInString:(NSString *)string {
+    NSArray *punctuationStrings = @[@".", @",", @";", @"-"];
+    
+    NSString *noPunctuation = string;
+    
+    for (NSString *punctuation in punctuationStrings) {
+        noPunctuation = [noPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *lowercaseNoPunctuation = [noPunctuation lowercaseString];
+    
+    NSArray *arrayOfWords = [lowercaseNoPunctuation componentsSeparatedByString:@" "];
+    
+    NSMutableDictionary *countsOfWords = [[NSMutableDictionary alloc] init];
+    
+    for (NSString *word in arrayOfWords) {
+        if ([countsOfWords[word] integerValue] > 0) {
+            NSInteger newCount = [countsOfWords[word] integerValue] + 1;
+            countsOfWords[word] = @(newCount);
+        } else {
+            countsOfWords[word] = @1;
+        }
+    }
+    
+    return [NSDictionary dictionaryWithDictionary:countsOfWords];
+}
+ 
+
+-(NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)array {
+    NSMutableDictionary *artistsWithSongs = [[NSMutableDictionary alloc] init];
+    
+    for (NSString *string in array) {
+        NSArray *artistAndSong = [string componentsSeparatedByString:@" - "];
+        NSString *artist = artistAndSong[0];
+        NSString *song = artistAndSong[1];
+        
+        if ([[artistsWithSongs allKeys] containsObject:artist]) {
+            [artistsWithSongs[artist] addObject:song];
+        } else {
+            artistsWithSongs[artist] = [ @[song] mutableCopy];
+        }
+    }
+    
+    NSSortDescriptor *sortByNilAsc = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES];
+    for (NSString *artist in artistsWithSongs) {
+        [artistsWithSongs[artist] sortUsingDescriptors:@[sortByNilAsc]];
+    }
+    
+    return [NSDictionary dictionaryWithDictionary:artistsWithSongs];
+}
 
 @end
